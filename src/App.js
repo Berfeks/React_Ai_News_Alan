1dcd1653353e59aa640ee3e9f146e60c1bd17062
Maksym Plevnytskyi <76871115+Berfeks@users.noreply.github.com>
2022-02-27 13:32:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import NewsCards from './components/NewsCards/NewsCards';
 import useStyles from './styles.js';
 import wordsToNumbers from 'words-to-numbers';
 
+
 const alanKey = '';
+
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
